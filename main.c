4291d4796dee59ba12351eb0a1eb23ee967fac84
arefer <kevin.arevalo@usach.cl>
2020-05-27 16:58:11 -0400

    Ejemplo de filtro r=0.

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include "image/image.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    char* path = argv[1];
     Image* img = (Image*)malloc(sizeof(Image));
-    Image_load(img, "Mozart.png");
+    load_image(img, path);
     if(img == NULL) {
         printf("Error in loading the image\n");
         exit(1);
     }
-    separate_channels(img);
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", img->width, img->height, img->channels);
-    printf("Pixel 328,229: (%d, %d, %d)", img->channels_data[0][328][229], img->channels_data[1][328][229], img->channels_data[2][328][229]);
-    Image_free(img);
+    separate_channels(img);
+    Pixel* p1 = &img->pixel_matrix[328][229];
+    printf("Pixel 328,229: (%d, %d, %d)", p1->r, p1->g, p1->b);
+
+    for (int row_index = 0 ; row_index < img->height ; row_index++){
+        for (int column_index = 0 ; column_index < img->width ; column_index++){
+            Pixel* p2 = &img->pixel_matrix[row_index][column_index];
+            p2->r = 0;
+        }
+    }
+    join_channels(img);
+    save_image(img, "Sin_r.jpg");
+    free_image(img);
     free(img);
     return 0;
 }
